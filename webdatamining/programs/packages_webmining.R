1729fe6d76552e7ab90d966c07b006c57d36c6ee
Gj Tchinde <jg.tchinde@gmail.com>
2014-11-09 23:40:09 +0100

    Packages for webmining

---
+++ webdatamining/programs/packages_webmining.R
@@ -0,0 +1,16 @@
+packages<-function(x){
+  x<-as.character(match.call()[[2]])
+  if (!require(x,character.only=TRUE)){
+    install.packages(pkgs=x,repos="http://cran.r-project.org")
+    require(x,character.only=TRUE)
+  }
+}
+packages(reshape2) 
+packages(ggplot2)
+packages(stringr)
+packages(tm)
+packages(RCurl)
+packages(stringdist)
+library(jsonlite)
+library(httr)
+library(dplyr)
